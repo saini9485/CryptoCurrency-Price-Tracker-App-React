74112bd381883ae4b124132cce50df31428712b8
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-09-29 16:42:22 +0530

    CrypotoCurrency Tracker App

---
+++ cryptocurrency-price-tracker-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import {Crypto} from "./components/Crypto"
+function App() {
+  return (
+   <div>
+<Crypto/>
+   </div>
+  )
+}
+
+export default App;
---
+++ cryptocurrency-price-tracker-app/src/components/Crypto.jsx
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import "./Crypto.css";
+import Coin from './Coin';
+import axios from "axios";
+export function Crypto() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setCoins([...response.data]);
+      });
+  }, []);
+
+
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  
+  return (
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
+          <button className="Search_Btn">Search Currency</button>
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.total_volume}
+            volume={coin.market_cap}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+
